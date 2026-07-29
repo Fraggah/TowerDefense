742edd1b1d29ccb573a7888cb7fb5c8995967b1f
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2026-07-29 17:10:43 -0300

    Add enemy basic movement

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,10 +1,11 @@
-using UnityEngine;
+ using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent agent;
 
+    [SerializeField] private float turnSpeed = 10;
     [SerializeField] private Transform[] waypoint;
     private int waypointIndex;
 
@@ -12,10 +13,13 @@ public class Enemy : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
+        agent.avoidancePriority = Mathf.RoundToInt(agent.speed * 10);
     }
 
     private void Update()
-    {   
+    {
+        FaceTarget(agent.steeringTarget);
+
         // Check if the agent is close to current target point
         if(agent.remainingDistance < .5f)
         {
@@ -26,12 +30,19 @@ public class Enemy : MonoBehaviour
 
     private void FaceTarget(Vector3 newTarget)
     {
+        // Calculate the direction from current position to the new target
+        Vector3 directionToTarget = newTarget - transform.position;
+        if (directionToTarget.magnitude == 0) return;
+        directionToTarget.y = 0; //Ignore any diference in the vertical position // Removes vertical component
 
+        Quaternion newRotation = Quaternion.LookRotation(directionToTarget);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, turnSpeed* Time.deltaTime);
     }
 
     private Vector3 GetNextWWaypoint()
     {
-        if (waypointIndex < waypoint.Length)
+        if (waypointIndex >= waypoint.Length)
         {
             return transform.position;  
         }
