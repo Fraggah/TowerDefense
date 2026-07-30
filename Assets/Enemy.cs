35bfa080e4cc6e16e1d0d9c87509056ca17d19b9
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2026-07-30 11:10:01 -0300

    Enemy Wave System

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,7 +6,8 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent agent;
 
     [SerializeField] private float turnSpeed = 10;
-    [SerializeField] private Transform[] waypoint;
+
+    [SerializeField] private Transform[] waypoints;
     private int waypointIndex;
 
     private void Awake()
@@ -14,6 +15,11 @@ public class Enemy : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.avoidancePriority = Mathf.RoundToInt(agent.speed * 10);
+    }
+
+    private void Start()
+    {
+        waypoints = FindAnyObjectByType<WaypointManager>().GetWaypoints();
     }
 
     private void Update()
@@ -42,11 +48,11 @@ public class Enemy : MonoBehaviour
 
     private Vector3 GetNextWWaypoint()
     {
-        if (waypointIndex >= waypoint.Length)
+        if (waypointIndex >= waypoints.Length)
         {
             return transform.position;  
         }
-        Vector3 targetPoint = waypoint[waypointIndex].position;
+        Vector3 targetPoint = waypoints[waypointIndex].position;
         waypointIndex++;
 
         return targetPoint;
