1e5d3073e2cbda513cd8adb58f2bfa59a9cc8a02
Silvio Stefanucci <silvioshredderstefanucci@gmail.com>
2026-07-31 15:27:19 -0300

    Adding Crosbow tower

---
+++ Assets/Scripts/Crossbow_Visuals.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using UnityEngine;
+
+public class Crossbow_Visuals : MonoBehaviour
+{
+    private Tower_Crossbow myTower;
+
+    [SerializeField] private LineRenderer attackVisuals;
+    [SerializeField] private float attackVisualDuration = .1f;
+
+    [Header("Glowing Visuals")]
+    [SerializeField] private MeshRenderer meshRenderer;
+    private Material material;
+
+    [Space]
+    private float currentIntensity;
+    [SerializeField] private float maxIntensity = 150;
+
+    [Space]
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color endColor;
+
+    private void Awake()
+    {
+        myTower = GetComponent<Tower_Crossbow>();
+
+        material = new Material(meshRenderer.material);
+
+        meshRenderer.material = material;
+        StartCoroutine(ChangeEmission(1));
+    }
+    public void PlayAttackVFX(Vector3 startPoint, Vector3 endPoint)
+    {
+        StartCoroutine(VFXCoroutine(startPoint, endPoint));
+    }
+
+    private void Update()
+    {
+        UpdateEmissionColor();
+    }
+
+    private void UpdateEmissionColor()
+    {
+        Color emissionColor = Color.Lerp(startColor, endColor, currentIntensity / maxIntensity);
+
+        emissionColor = emissionColor * Mathf.LinearToGammaSpace(currentIntensity);
+
+        material.SetColor("_EmissionColor", emissionColor);
+    }
+
+    public void PlayReloadFX(float duration)
+    {
+        StartCoroutine(ChangeEmission(duration / 2));
+    }
+
+    private IEnumerator VFXCoroutine(Vector3 startPoint, Vector3 endPoint)
+    {
+        myTower.EnableRotation(false);
+
+        attackVisuals.enabled = true;
+        attackVisuals.SetPosition(0, startPoint);
+        attackVisuals.SetPosition(1, endPoint);
+
+        yield return new WaitForSeconds(attackVisualDuration);
+
+        attackVisuals.enabled = false;
+        myTower.EnableRotation(true);
+    }
+
+    private IEnumerator ChangeEmission(float duration)
+    {
+        float startTime = Time.time;
+        float startIntensity = 0;
+
+        while (Time.time - startTime < duration)
+        {
+            float tValue = (Time.time - startTime) / duration;
+            currentIntensity = Mathf.Lerp(startIntensity, maxIntensity, tValue);
+            yield return null;
+        }
+
+        currentIntensity = maxIntensity;
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public Transform currentEnemy;
+
+    [SerializeField] protected float attackCooldown = 1;
+    protected float lastTimeAttacked;
+
+    [Header("Tower Setup")]
+    [SerializeField] protected Transform towerHead;
+    [SerializeField] protected float rotationSpeed = 10;
+    private bool canRotate;
+
+    [SerializeField] protected float attackRange = 2.5f;
+    [SerializeField] protected LayerMask whatIsEnemy;
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        if (currentEnemy == null)
+        {
+            currentEnemy = FindRandomEnemyWithinRange();
+            return;
+        }
+
+        if (CanAttack()) Attack();
+
+        if (Vector3.Distance(currentEnemy.position, transform.position) > attackRange)
+        {
+            currentEnemy = null;
+        }
+
+        RotateTowardsEnemy(); 
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+
+    protected bool CanAttack()
+    {
+        if (Time.time > lastTimeAttacked + attackCooldown)
+        {
+            lastTimeAttacked = Time.time;
+            return true;
+        }
+
+        return false;
+    }
+
+    protected Transform FindRandomEnemyWithinRange()
+    {
+        List<Transform> possibleTargets = new List<Transform>();
+        Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange, whatIsEnemy);
+
+        foreach (Collider enemy in enemiesAround)
+        {
+            possibleTargets.Add(enemy.transform);
+        }
+
+        int randomIndex = Random.Range(0, possibleTargets.Count);
+
+        if (possibleTargets.Count <= 0) return null;
+
+        return possibleTargets[randomIndex];
+    }
+
+    public void EnableRotation(bool enable)
+    {
+        canRotate = enable;
+    }
+
+    protected virtual void RotateTowardsEnemy()
+    {
+        if (!canRotate) return;
+        if (currentEnemy == null) return;
+
+        Vector3 directionToEnemy = currentEnemy.position - towerHead.position;
+
+        Quaternion lookRotation = Quaternion.LookRotation(directionToEnemy);
+
+        Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, rotationSpeed * Time.deltaTime).eulerAngles;
+
+        towerHead.rotation = Quaternion.Euler(rotation);
+    }
+
+    protected Vector3 DirectionToEnemyFrom(Transform startPoint)
+    {
+        return (currentEnemy.position - startPoint.position).normalized;
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
